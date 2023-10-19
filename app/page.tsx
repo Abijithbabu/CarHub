d3e54ac2867e02439840ac367e30bec6f64c3a1b
Abhijith Babu <abijithb2002@gmail.com>
2023-10-19 20:08:51 +0530

    minor updation

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,21 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
+        </div>
+
+        <div className='home__filters'>
+          {/* <SearchBar /> */}
+
+          <div className='home__filter-container'>
+            {/* <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} /> */}
+          </div>
+        </div>
+        </div>
     </main>
   )
 }
